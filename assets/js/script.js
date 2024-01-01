57a7942e630bea7f8834198020573fd89dc04d0a
DilaraUcar <>
2024-01-01 16:45:07 +0000

    Add game questions and answers to js

---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+const quizData = [
+    {
+        question: 'What is the capital of France?',
+        options: ['Berlin', 'Madrid', 'Paris', 'Rome'],
+        correctAnswer: 'Paris'
+    },
+    {
+        question: 'Which river is the longest in the world?',
+        options: ['Amazon', 'Nile', 'Yangtze', 'Mississippi'],
+        correctAnswer: 'Nile'
+    },
+    {
+        question: 'What is the capital of Sweden?',
+        options: ['Oslo', 'Copenhagen', 'Gothenburg', 'Stockholm'],
+        correctAnswer: 'Stockholm'
+    },
+    {
+        question: 'What is the name of the larges Country in the world?',
+        options: ['China', 'Russia', 'United States', 'Canada'],
+        correctAnswer: 'Russia'
+    },
+    {
+        question: 'What is the capital of Canada?',
+        options: ['Ottawa', 'Toronto', 'Sofia', 'Canberra'],
+        correctAnswer: 'Ottawa'
+    },
+    {
+        question: 'What is the flattest country on Earth?',
+        options: ['France', 'The Maldives', 'Sweden', 'Thailand'],
+        correctAnswer: 'The Maldives'
+    },
+    {
+        question: 'What is the capital of Norway?',
+        options: ['Stockholm', 'Oslo', 'Helsinki', 'Copenhagen'],
+        correctAnswer: 'Oslo'
+    },
+    {
+        question: 'What is the tallest mountain in the world?',
+        options: ['Lhotse', 'Kebnekaise', 'Mount Everest', 'K2'],
+        correctAnswer: 'Mount Everest'
+    },
+    {
+        question: 'Which country has the largest population in the world?',
+        options: ['India', 'Russia', 'Thailand', 'China'],
+        correctAnswer: 'China'
+    },
+    {
+        question: 'What is the capital of Mexico?',
+        options: ['Mexico', 'Mexico City', 'Madrid', 'Texas'],
+        correctAnswer: 'Mexico City'
+    }
+    // Add more questions 
+];
